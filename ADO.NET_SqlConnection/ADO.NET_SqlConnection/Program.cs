4e8eb9c9d6064b61d1b647ba0104eace3561ea86
padoleshital <shitalpadole7@gmail.com>
2025-09-23 01:05:39 +0530

    ADO.Net Connetion program

---
+++ ADO.NET_SqlConnection/ADO.NET_SqlConnection/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.Connection();
+            Console.ReadLine();
+        }
+       static void Connection()
+        {
+        string connectionString = "Data Source=LAPTOP-INKR4433\\SQLEXPRESS;Initial Catalog=Practice;Integrated Security=True;TrustServerCertificate=True;";
+        SqlConnection conn = new SqlConnection(connectionString);
+
+        try
+        {
+            conn.Open();
+
+            if (conn.State == ConnectionState.Open)
+            {
+                Console.WriteLine("Connection is Establisd");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            conn.Close();
+        }
+       
+           
+
+        conn.Close();
+
+        }
+    }
